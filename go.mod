313c632a56099b11368df0f95c7e06ee42eb9171
Jerome Grassnick codefuzzler <admin@codefuzzler.xyz>
2020-12-14 16:19:11 +0100

    Implemented argument to Config parsing. Implemented google drive api libs. Implemented parsing credentials.json. Implemented OAuth2 via provided link. srcFolder gets archived with tar and compressed with gunzip and correctly named with descriptor and time. Errors should be handled if occuring. Implemented auth, connect, createdir and fileupload to google drive. First release candidate.

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module gdrive-backup
 go 1.15
 
 require (
+	cloud.google.com/go v0.74.0 // indirect
 	golang.org/x/net v0.0.0-20201209123823-ac852fbbde11
 	golang.org/x/oauth2 v0.0.0-20201208152858-08078c50e5b5
+	golang.org/x/sys v0.0.0-20201214095126-aec9a390925b // indirect
 	google.golang.org/api v0.36.0
+	google.golang.org/genproto v0.0.0-20201211151036-40ec1c210f7a // indirect
 )
--- main.go
+++ main.go
@@ -4,13 +4,21 @@ import (
 	"archive/tar"
 	"bufio"
 	"compress/gzip"
+	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
 	"time"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/drive/v3"
 )
 
 var Conf Config
@@ -20,9 +28,9 @@ func init() {
 }
 
 func main() {
+	fmt.Println("Google drive backup utility.")
 	fmt.Printf(
-		"Args: %s %s %s %s\n",
-		Conf.ApiKey,
+		"Args:\n Source: %s\n Destination: %s\n Descriptor: %s\n\n",
 		Conf.Source,
 		Conf.Destination,
 		Conf.Descriptor,
@@ -30,7 +38,33 @@ func main() {
 
 	var tarFile = srcToTar(Conf.Source)
 	tarFile.Close()
-	fmt.Println(fmt.Sprintf("Temporary file: %s", tarFile.Name()))
+	fmt.Println(fmt.Sprintf("Backup Archive: %s", tarFile.Name()))
+
+	// Step 1. Open the file
+	f, err := os.Open(tarFile.Name())
+
+	if err != nil {
+		processError(err, "Cannot open backup archive.")
+	}
+
+	defer f.Close()
+
+	// Step 2. Get the Google Drive service
+	service, err := getService()
+
+	// Step 3. Create the directory
+	dir, err := createDir(service, Conf.Destination, "root")
+
+	if err != nil {
+		processError(err, "Could not create remote directory.")
+	}
+
+	// Step 4. Create the file and upload its content
+	file, err := createFile(service, tarFile.Name(), "image/png", f, dir.Id)
+
+	if err != nil {
+		processError(err, fmt.Sprintf("Could not create file: %s\n", file.Name))
+	}
 }
 
 func srcToTar(src string) os.File {
@@ -38,40 +72,42 @@ func srcToTar(src string) os.File {
 	tmpName := fmt.Sprintf("./tmp_" + Conf.Descriptor + dt.Format("02-01-2006_15:04:05") + ".tar.gz")
 	file, err := os.Create(tmpName)
 	if err != nil {
-		processError(err)
+		processError(err, fmt.Sprintf("Could not create file: %s", tmpName))
 	}
 	writer := bufio.NewWriter(file)
 	err = Tar(src, writer)
 	if err != nil {
-		processError(err)
+		processError(err, fmt.Sprintf("Error writing to file: %s", file.Name()))
 	}
 	writer.Flush()
 	file.Close()
 	return *file
 }
 
-func processError(err error) {
+func processError(err error, msg string) {
 	flag.PrintDefaults()
-	fmt.Print(err)
+	fmt.Println(fmt.Errorf("Error!\nMsg: %s\nErr: %s\n", msg, err))
 	os.Exit(2)
 }
 
+type Credentials struct {
+	ClientID string
+	Secret   string
+}
+
 type Config struct {
-	ApiKey      string
 	Source      string
 	Destination string
 	Descriptor  string
 }
 
 func parseArguments() Config {
-	var ApiKey = flag.String("key", "", "Your GoogleDrive API Key.")
 	var Source = flag.String("src", ".", "The source folder to backup.")
 	var Destination = flag.String("dest", "/backup", "The GoogleDrive backup destination folder.")
 	var Descriptor = flag.String("tag", "", "The descriptor tag which will be included in the filename.")
 	flag.Parse()
 
 	return Config{
-		ApiKey:      *ApiKey,
 		Source:      *Source,
 		Destination: *Destination,
 		Descriptor:  *Descriptor,
@@ -141,4 +177,120 @@ func Tar(src string, writers ...io.Writer) error {
 
 		return nil
 	})
+}
+
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		processError(err, "Unable to read authorization code")
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		processError(err, "Unable to retrieve token from web")
+	}
+	return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		//processError(err, "")
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		processError(err, "Unable to cache oauth token.")
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+func getService() (*drive.Service, error) {
+	b, err := ioutil.ReadFile("credentials.json")
+	if err != nil {
+		processError(err, "Unable to read credentials.json file.")
+		return nil, err
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, drive.DriveFileScope)
+
+	if err != nil {
+		processError(err, "Unable to delete your previously saved token.json.")
+		return nil, err
+	}
+
+	client := getClient(config)
+
+	service, err := drive.New(client)
+
+	if err != nil {
+		processError(err, "Cannot create the Google Drive service.")
+		return nil, err
+	}
+
+	return service, err
+}
+
+func createDir(service *drive.Service, name string, parentId string) (*drive.File, error) {
+	d := &drive.File{
+		Name:     name,
+		MimeType: "application/vnd.google-apps.folder",
+		Parents:  []string{parentId},
+	}
+
+	file, err := service.Files.Create(d).Do()
+
+	if err != nil {
+		processError(err, "Could not create dir.")
+		return nil, err
+	}
+
+	return file, nil
+}
+
+func createFile(service *drive.Service, name string, mimeType string, content io.Reader, parentId string) (*drive.File, error) {
+	f := &drive.File{
+		MimeType: mimeType,
+		Name:     name,
+		Parents:  []string{parentId},
+	}
+	file, err := service.Files.Create(f).Media(content).Do()
+
+	if err != nil {
+		processError(err, fmt.Sprintf("Could not create file."))
+		return nil, err
+	}
+
+	return file, nil
 }
